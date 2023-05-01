93475784328b723f24308bd3286cc86b356f5ede
ImRan Huang <hyp117@whut.edu.cn>
2023-05-01 22:19:32 +0800

    feat:数组复制
    --把一个数组中的元素复制到另一个新数组中去

--- day07-code/src/Test4.java
+++ day07-code/src/Test4.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Test4 {
+public class Test4 {
+    public static void main(String[] args) {
+
+        /*需求:
+把一个数组中的元素复制到另一个新数组中去。*/
+//分析:
+
+// 1.定义一个老数组并存储一些元素
+int[] arr={1,2,3,4,5};
+
+
+
+//2.定义一个新数组的长度跟老数组一致
+
+int[] newArr=new int[arr.length];
+
+
+//3.遍历老数组，得到老数组中的每一个元素，依次存入到新数组当中
+
+        for (int i = 0; i < arr.length; i++) {
+           // System.out.println(arr[i]);
+            newArr[i]=arr[i];
+        }
+
+
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.println(newArr[i]);
+        }
+
+
+
+
+
+
+
+    }
 }
